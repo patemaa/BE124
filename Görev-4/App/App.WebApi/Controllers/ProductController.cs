934cd1c5c5ae09fe1d5042c144e44b1e76a324de
patemaa <137816409+patemaa@users.noreply.github.com>
2024-08-12 18:25:01 +0300

    Görev-4 tamamlandı.

---
+++ Görev-4/App/App.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        static private readonly List<Product> _products = new();
+
+        [HttpGet]
+        public IEnumerable<Product> GetList()
+        {
+            return _products;
+        }
+
+        [HttpGet("{id}")]
+        public Product? GetProduct(int id)
+        {
+            return _products.Find(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public void AddProduct(Product product)
+        {
+            product.Id = _products.Count + 1;
+            _products.Add(product);
+        }
+
+        [HttpPut("{id}")]
+        public void UpdateProduct(int id, string name, decimal price, string category)
+        {
+            var index = _products.FindIndex(x => x.Id == id);
+
+            if (index == -1)
+            {
+                return;
+            }
+            _products[index].Name = name;
+            _products[index].Price = price;
+            _products[index].Category = category;
+
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteProduct(int id)
+        {
+            var index = _products.FindIndex(x => x.Id == id);
+            if (index == -1)
+            {
+                return;
+            }
+            _products.RemoveAt(index);
+        }
+    }
+}
